0c0079a8c450dcc27eacb66fd8a5d2e1a422ed10
S.Tovey <tovey.samuel@gmail.com>
2021-12-16 14:22:15 +0100

    Update the example script.

--- examples/simple_spheres.py
+++ examples/simple_spheres.py
@@ -27,14 +27,18 @@ if __name__ == '__main__':
     """
     Run the simple spheres example.
     """
-    trajectory = np.random.uniform(-10, 10, (1000, 10, 3))
-    trajectory_2 = np.random.uniform(10, 20, (1000, 10, 3))
+    # Define the first particle.
+    trajectory = np.random.uniform(-10, 10, (1000, 50, 3))
     mesh = vis.Sphere(radius=2.0, colour=np.array([30, 144, 255]) / 255, resolution=10)
+    particle = vis.Particle(name="Blue", mesh=mesh, position=trajectory)
+
+    # Define the second particle.
+    trajectory_2 = np.random.uniform(10, 30, (1000, 50, 3))
     mesh_2 = vis.Sphere(
         radius=2.0, colour=np.array([255, 140, 0]) / 255, resolution=10
     )
-    particle = vis.Particle(name="Ball", mesh=mesh, position=trajectory)
-    particle_2 = vis.Particle(name="Ball_O", mesh=mesh_2, position=trajectory_2)
+    particle_2 = vis.Particle(name="Orange", mesh=mesh_2, position=trajectory_2)
 
-    visualizer = vis.Visualizer(particles=[particle, particle_2])
+    # Construct the visualizer and run
+    visualizer = vis.Visualizer(particles=[particle, particle_2], frame_rate=50)
     visualizer.run_visualization()
