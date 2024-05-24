143596b4cd606763080dee1c77a87e562b40ead2
SamTov <tovey.samuel@gmail.com>
2024-05-24 16:30:30 +0100

    Increase scale of simple spheres example

--- examples/simple_spheres.py
+++ examples/simple_spheres.py
@@ -31,8 +31,8 @@ if __name__ == "__main__":
     """
     material_1 = vis.Material(colour=np.array([30, 144, 255]) / 255, alpha=0.6)
     # Define the first particle.
-    trajectory = np.random.uniform(-100, 100, (100, 1000, 3))
-    mesh = vis.Sphere(radius=2.0, resolution=10, material=material_1)
+    trajectory = np.random.uniform(-100, 100, (100, 10000, 3))
+    mesh = vis.Sphere(radius=2.0, resolution=3, material=material_1)
     particle = vis.Particle(
         name="Blue", mesh=mesh, position=trajectory, smoothing=False
     )
@@ -40,8 +40,8 @@ if __name__ == "__main__":
     material_2 = vis.Material(colour=np.array([255, 140, 0]) / 255, alpha=1.0)
 
     # Define the second particle.
-    trajectory_2 = np.random.uniform(-10, 10, (100, 1000, 3))
-    mesh_2 = vis.Sphere(radius=1.0, resolution=10, material=material_2)
+    trajectory_2 = np.random.uniform(-10, 10, (100, 10000, 3))
+    mesh_2 = vis.Sphere(radius=1.0, resolution=3, material=material_2)
     particle_2 = vis.Particle(
         name="Orange", mesh=mesh_2, position=trajectory_2, smoothing=False
     )
