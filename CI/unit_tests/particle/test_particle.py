fcd75475a6920b5c4117667fc9f2908af7c401ad
jpd-de <st177081@stud.uni-stuttgart.de>
2025-04-24 14:17:28 +0200

    Fix the variable particle number features
    Also added the related tests.

--- CI/unit_tests/particle/test_particle.py
+++ CI/unit_tests/particle/test_particle.py
@@ -43,10 +43,66 @@ class TestParticle(unittest.TestCase):
         -------
 
         """
-        cls.material = Material()
+        cls.material = Material(colour=np.zeros((10, 10, 3)))
         name = "my_particle"
         position = np.random.uniform(-5, 5, (10, 2, 3))
-        cls.particle = Particle(name=name, position=position, mesh=Sphere())
+        director = np.random.uniform(-5, 5, (10, 2, 3))
+        cls.particle = Particle(
+            name=name,
+            position=position,
+            director=director,
+            mesh=Sphere(material=cls.material),
+        )
+
+        static_name = "my_static_particle"
+        static_position = np.random.uniform(-5, 5, (2, 3))
+        static_director = np.random.uniform(-5, 5, (2, 3))
+        cls.static_particle = Particle(
+            name=static_name,
+            position=static_position,
+            director=static_director,
+            mesh=Sphere(),
+            static=True,
+            smoothing=True,
+        )
+
+        empty_name = "my_empty_particle"
+        cls.empty_particle = Particle(
+            name=empty_name, mesh=Sphere(), position=np.array([]), director=np.array([])
+        )
+
+        nan_name = "my_nan_particle"
+        nan_position = [np.full((2, 3), np.nan) for _ in range(10)]
+        cls.nan_particle = Particle(
+            name=nan_name,
+            mesh=Sphere(),
+            position=nan_position,
+            director=nan_position.copy(),
+        )
+
+        configuration_warning_name = "my_static_mixing_particle"
+        configuration_warning_position = np.random.uniform(-5, 5, (1, 2, 3))
+        configuration_warning_director = np.random.uniform(-5, 5, (1, 2, 3))
+        cls.configuration_warning_particle = Particle(
+            name=configuration_warning_name,
+            position=configuration_warning_position,
+            director=configuration_warning_director,
+            mesh=Sphere(),
+            static=True,
+            smoothing=True,
+        )
+
+    def test_configuration_warning(self):
+        """
+        Test the construct_mesh_list method for a static mesh.
+
+        Returns
+        -------
+        Tests whether the list was created properly.
+        """
+        self.configuration_warning_particle.construct_mesh_list()
+        assert self.configuration_warning_particle.position[0].shape == (2, 3)
+        assert self.configuration_warning_particle.director[0].shape == (2, 3)
 
     def test_initialization(self):
         """
@@ -58,18 +114,94 @@ class TestParticle(unittest.TestCase):
         """
         self.assertEqual(type(self.particle.mesh), Sphere)
         self.assertEqual(self.particle.name, "my_particle")
-        np.testing.assert_array_equal(self.particle.position.shape, (10, 2, 3))
+        self.assertEqual(len(self.particle.position), 10)
+        np.testing.assert_array_equal(self.particle.position[0].shape, (2, 3))
 
-    def test_construct_mesh_dict(self):
+    def test_construct_mesh_list(self):
         """
-        Test the construct_mesh_dict method.
+        Test the construct_mesh_list method.
 
         Returns
         -------
-        Tests whether the dict was created properly.
+        Tests whether the list was created properly.
         """
-        # Build the mesh dict
+        # Build the mesh list
         self.particle.construct_mesh_list()
 
-        # Check that all time steps are in the dict.
-        self.assertEqual(len(self.particle.mesh_list), self.particle.position.shape[0])
+        # Check that all time steps are in the list.
+        self.assertEqual(len(self.particle.mesh_list), len(self.particle.position))
+
+    def test_static_initialization(self):
+        """
+        Test the initialization of the class.
+
+        Returns
+        -------
+        Check if the attributes are set correctly.
+        """
+        self.assertEqual(type(self.static_particle.mesh), Sphere)
+        self.assertEqual(self.static_particle.name, "my_static_particle")
+        # NOTE
+        # What behavior is needed here?
+        # np.testing.assert_array_equal(self.static_particle.position.shape, (1, 2, 3))
+        self.assertTrue(self.static_particle.static)
+        self.assertTrue(self.static_particle.smoothing)
+
+    def test_construct_static_mesh_list(self):
+        """
+        Test the construct_mesh_list method for a static mesh.
+
+        Returns
+        -------
+        Tests whether the list was created properly.
+        """
+        self.assertEqual(np.asarray(self.static_particle.position).shape, (2, 3))
+        self.static_particle.construct_mesh_list()
+
+        self.assertEqual(np.asarray(self.static_particle.position)[0].shape, (2, 3))
+
+    def test_empty_initialization(self):
+        """
+        Test the initialization of the class.
+
+        Returns
+        -------
+        Check if the attributes are set correctly.
+        """
+        self.assertEqual(type(self.empty_particle.mesh), Sphere)
+        self.assertEqual(self.empty_particle.name, "my_empty_particle")
+
+    def test_construct_empty_mesh_list(self):
+        """
+        Test the construct_mesh_list method for an empty mesh.
+
+        Returns
+        -------
+        Tests if the ValueError is thrown.
+        """
+        with self.assertRaises((ValueError)) as context:
+            self.empty_particle.construct_mesh_list()
+        # Check if error message is correct
+        self.assertIn("The provided position array is empty.", str(context.exception))
+
+        self.empty_particle.position = np.random.uniform(-5, 5, (2, 2, 3))
+        self.empty_particle.construct_mesh_list()
+        assert self.empty_particle.director is None
+
+    def test_construct_nan_mesh_list(self):
+        """
+        Test the construct_mesh_list method for a particle with nans in the trajectory.
+
+        Returns
+        -------
+        Tests whether the list was created properly.
+        """
+        with self.assertRaises((ValueError)) as context:
+            self.nan_particle.construct_mesh_list()
+        self.assertIn("The provided position data contains NaN", str(context.exception))
+
+        self.nan_particle.position = np.random.uniform(-5, 5, (2, 2, 3))
+        self.nan_particle.director = np.full((2, 2, 3), np.nan)
+        with self.assertRaises((ValueError)) as context:
+            self.nan_particle.construct_mesh_list()
+        self.assertIn("The provided director data contains NaN", str(context.exception))
--- znvis/particle/particle.py
+++ znvis/particle/particle.py
@@ -41,20 +41,24 @@ class Particle:
             Name of the particle
     mesh : Mesh
             Mesh to use e.g. sphere
-    position : np.ndarray
-            Position tensor of the shape (n_confs, n_particles, n_dims)
-    velocity : np.ndarray
-            Velocity tensor of the shape (n_confs, n_particles, n_dims)
-    force : np.ndarray
-            Force tensor of the shape (n_confs, n_particles, n_dims)
-    director: np.ndarray
-            Director tensor of the shape (n_confs, n_particles, n_dims)
+    position : Union(np.ndarray, list of np.ndarray)
+            Position tensor of either (n_confs, n_particles, n_dims) or
+            list of (n_particles_t, n_dims) with len n_confs
+    velocity : Union[np.ndarray, list of np.ndarray]
+            Velocity tensor of either (n_confs, n_particles, n_dims) or
+            list of (n_particles_t, n_dims) with len n_confs
+    force : Union[np.ndarray, list of np.ndarray]
+            Force tensor of either (n_confs, n_particles, n_dims) or
+            list of (n_particles_t, n_dims) with len n_confs
+    director: Union[np.ndarray, list of np.ndarray]
+            Director tensor of either (n_confs, n_particles, n_dims) or
+            list of (n_particles_t, n_dims) with len n_confs
     mesh_list : list
             A list of mesh objects, one for each time step.
     static : bool (default=False)
             If true, only render the mesh once at initialization. Be careful
             as this changes the shape of the required position and director
-            to (n_particles, n_dims)
+            to (n_particles, n_dims).
 
     smoothing : bool (default=False)
             If true, apply smoothing to each mesh object as it is rendered.
@@ -64,10 +68,10 @@ class Particle:
 
     name: str
     mesh: Mesh = None
-    position: np.ndarray = None
-    velocity: np.ndarray = None
-    force: np.ndarray = None
-    director: np.ndarray = None
+    position: typing.Union[np.ndarray, typing.List[np.ndarray]] = None
+    velocity: typing.Union[np.ndarray, typing.List[np.ndarray]] = None
+    force: typing.Union[np.ndarray, typing.List[np.ndarray]] = None
+    director: typing.Union[np.ndarray, typing.List[np.ndarray]] = None
     mesh_list: typing.List[Mesh] = None
     static: bool = False
     smoothing: bool = False
@@ -113,35 +117,85 @@ class Particle:
         Updates the class attributes mesh_list
         """
         self.mesh_list = []
-        try:
+
+        # Convert ndarrays into lists
+        if isinstance(self.position, np.ndarray):
+            if self.position.size == 0:
+                raise ValueError(
+                    "The provided position array is empty.",
+                    "Please provide a valid position array.",
+                )
+            if self.director is not None and self.director.size == 0:
+                self.director = None
+                print(
+                    "-------\nWARNING: The provided director array is empty.",
+                    "Setting to None.\n-------",
+                )
+            # Static case differentiation
             if not self.static:
-                n_particles = int(self.position.shape[1])
-                n_time_steps = int(self.position.shape[0])
-            else:
-                n_particles = int(self.position.shape[0])
-                n_time_steps = 1
-                self.position = self.position[np.newaxis, :, :]
+                n_time_steps = self.position.shape[0]
+                self.position = [self.position[i] for i in range(n_time_steps)]
                 if self.director is not None:
-                    self.director = self.director[np.newaxis, :, :]
+                    self.director = [self.director[i] for i in range(n_time_steps)]
+            else:
+                n_time_steps = 1
+                if self.position.ndim == 3:
+                    print(
+                        "-------\nWARNING: The provided position array has an ",
+                        "unexpected shape. Using the first entry as the static array.",
+                        "\n-------",
+                    )
+                    self.position = [self.position[0, :, :]]
+                elif self.position.ndim == 2:
+                    self.position = [self.position]
 
-        except ValueError:
-            raise ValueError("There is no data for these particles.")
-
-        for i in track(range(n_time_steps), description=f"Building {self.name} Mesh"):
-            for j in range(n_particles):
-                if j == 0:
-                    if self.director is not None:
-                        mesh = self._create_mesh(
-                            self.position[i][j], self.director[i][j], i, j
+                if self.director is not None:
+                    if self.director.ndim == 3:
+                        self.director = [self.director[0, :, :]]
+                        print(
+                            "-------\nWARNING: The provided director array has an ",
+                            "unexpected shape. Using the first entry as the static ",
+                            "array.\n-------",
                         )
-                    else:
-                        mesh = self._create_mesh(self.position[i][j], None, i, j)
-                else:
-                    if self.director is not None:
-                        mesh += self._create_mesh(
-                            self.position[i][j], self.director[i][j], i, j
-                        )
-                    else:
-                        mesh += self._create_mesh(self.position[i][j], None, i, j)
+                    elif self.director.ndim == 2:
+                        self.director = [self.director]
+        # List case
+        else:
+            n_time_steps = len(self.position)
 
-            self.mesh_list.append(mesh)
+        # Check data for consistency
+        for i, position in enumerate(self.position):
+            if np.isnan(position).any():
+                raise ValueError(
+                    f"The provided position data contains NaN values at time step {i}."
+                )
+        if self.director:
+            for i, director in enumerate(self.director):
+                if director is not None and np.isnan(director).any():
+                    raise ValueError(
+                        "The provided director data contains NaN ",
+                        f"values at time step {i}.",
+                    )
+
+        # Create the mesh
+        for frame_index in track(
+            range(n_time_steps), description=f"Building {self.name} Mesh"
+        ):
+            frame_pos = self.position[frame_index]
+            frame_dir = (
+                self.director[frame_index] if self.director is not None else None
+            )
+            n_particles = frame_pos.shape[0]
+            meshes = []
+            for particle_index in range(n_particles):
+                pos = frame_pos[particle_index]
+                dir = frame_dir[particle_index] if frame_dir is not None else None
+                mesh = self._create_mesh(pos, dir, frame_index, particle_index)
+                meshes.append(mesh)
+
+            # Combine all meshes into one
+            combined_mesh = meshes[0]
+            for m in meshes[1:]:
+                combined_mesh += m
+
+            self.mesh_list.append(combined_mesh)
